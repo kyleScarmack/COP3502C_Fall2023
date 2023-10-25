392e4caf9cb3b291dd764fe28d43a1c5d6561866
kylescarmack <scarmackkyle@gmail.com>
2023-10-25 18:10:50 -0400

    main function

---
+++ main.py
@@ -0,0 +1,60 @@
+"""
+Lab 6
+Authors: Kyle Scarmack
+Class: COP3502C
+Section: 12309
+Description: A simplified password encoder/decoder program
+"""
+
+
+def menu():
+    # print menu options
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print()
+
+
+def encode(password):
+    encoded_password = ""
+    for num in password:
+        # shift each digit up by 3 numbers
+        encoded_num = str(int(num) + 3)
+        # if the encoded number is 10 or more, the encoded number should only display the 2nd digit, such as 10 being 0
+        if int(encoded_num) > 9:
+            encoded_num = str(int(encoded_num) - 10)
+        # add encoded number to encoded password
+        encoded_password += encoded_num
+    return encoded_password
+
+
+def decode():
+    pass
+
+
+def main():
+    password = None
+    encoded_password = None
+    decoded_password = None
+    user_selection = True
+    while user_selection:
+        menu()
+        # prompt user for menu option
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            # prompt user to enter password
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!\n")
+        elif option == 2:
+            # display encoded password and decoded password
+            # decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password} and the original password is {decoded_password}.\n")
+        elif option == 3:
+            quit()
+
+
+if __name__ == "__main__":
+    main()
